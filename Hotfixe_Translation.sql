b714dc9200e3496b7e14a1d8c0bb663cd8f789c4
Wishmaster117 <140754794+Wishmaster117@users.noreply.github.com>
2024-10-30 14:51:33 +0100

    Update Hotfixe_Translation.sql

--- Hotfixe_Translation.sql
+++ Hotfixe_Translation.sql
@@ -1,1 +1,15 @@
+SET
+@ENTRY          := 9000000,
+@NAME           := "Portal Master",
+@SUBNAME        := "Where do you go",
+@MODEL          := 21572,
 
+@AURA           := "30540", -- "35766" = casting
+
+@TEXT_ID        := 900000,
+@GOSSIP_MENU    := 200000,
+@BROAD_TEXT     := 900000,
+@VBUILD         := 56819,
+@RUNE           := 194394;
+
+-- Deleting code
