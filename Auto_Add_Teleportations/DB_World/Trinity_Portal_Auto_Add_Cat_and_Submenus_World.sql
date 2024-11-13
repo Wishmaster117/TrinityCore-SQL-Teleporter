81a60b8ee526d8b49ca7f09de82d52906a900666
Wishmaster117 <140754794+Wishmaster117@users.noreply.github.com>
2024-11-13 14:07:15 +0100

    Update Trinity_Portal_Auto_Add_Cat_and_Submenus_World.sql

--- Auto_Add_Teleportations/DB_World/Trinity_Portal_Auto_Add_Cat_and_Submenus_World.sql
+++ Auto_Add_Teleportations/DB_World/Trinity_Portal_Auto_Add_Cat_and_Submenus_World.sql
@@ -129,37 +129,6 @@ INSERT INTO `gossip_menu_option_locale` (`MenuID`, `OptionID`, `Locale`, `Option
 (@SUB_MENU, @SUB_MENU_BACK_ORDER, 'zhCN', "返回..", NULL),
 (@SUB_MENU, @SUB_MENU_BACK_ORDER, 'zhTW', "返回..", NULL);
 
--- -------------------------------------- --
---            Hotfixes DB                 --
--- -------------------------------------- --
-
--- Calcul de @NEW_BROAD_TEXT_ID basé sur la dernière valeur de ID dans la fourchette [9000000, 9000500]
-SET @NEW_BROAD_TEXT_ID := (
-    SELECT COALESCE(MAX(ID), @TEXT_ID) + 1
-    FROM broadcast_text
-    WHERE ID BETWEEN @TEXT_ID AND 9000500
-);
-
--- Hotfix broadcast_text table -- Submenu header
-INSERT INTO `broadcast_text` (`Text`, `Text1`, `ID`, `LanguageID`, `ConditionID`, `EmotesID`, `Flags`, `ChatBubbleDurationMs`, `VoiceOverPriorityID`, `SoundKitID1`, `SoundKitID2`, `EmoteID1`, `EmoteID2`, `EmoteID3`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `VerifiedBuild`) VALUES
-(@BROADCAST_TEXT , '', @NEW_BROAD_TEXT_ID, 0, 0, 0, 68, 0, 0, 58268, 0, 0, 0, 0, 0, 0, 0, @VBUILD);
-
--- Hotfix broadcast_text table -- Submenu translation
-INSERT INTO `broadcast_text_locale` (`ID`, `locale`, `Text_lang`, `Text1_lang`, `VerifiedBuild`) VALUES 
-(@NEW_BROAD_TEXT_ID, 'deDE', '$B ADD TRANSLATION $B', '', @VBUILD),
-(@NEW_BROAD_TEXT_ID, 'esES', '$B ADD TRANSLATION $B', '', @VBUILD),
-(@NEW_BROAD_TEXT_ID, 'esMX', '$B ADD TRANSLATION $B', '', @VBUILD),
-(@NEW_BROAD_TEXT_ID, 'frFR', '$B ADD TRANSLATION $B', '', @VBUILD),
-(@NEW_BROAD_TEXT_ID, 'itIT', '$B ADD TRANSLATION $B', '', @VBUILD),
-(@NEW_BROAD_TEXT_ID, 'koKR', '$B ADD TRANSLATION $B', '', @VBUILD),
-(@NEW_BROAD_TEXT_ID, 'ptBR', '$B ADD TRANSLATION $B', '', @VBUILD),
-(@NEW_BROAD_TEXT_ID, 'ruRU', '$B ADD TRANSLATION $B', '', @VBUILD),
-(@NEW_BROAD_TEXT_ID, 'zhCN', '$B ADD TRANSLATION $B', '', @VBUILD),
-(@NEW_BROAD_TEXT_ID, 'zhTW', '$B ADD TRANSLATION $B', '', @VBUILD);
-
-
-
-
 
 -- Add submenus to a category
 
